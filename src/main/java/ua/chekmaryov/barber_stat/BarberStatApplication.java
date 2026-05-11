79778746e0f4df9f0d3206109b752fab9d09ea07
izanami <pochtovik1941@gmail.com>
2026-05-11 04:00:50 +0300

    Changed location of file

--- src/main/java/ua/chekmaryov/barber_stat/BarberStatApplication.java
+++ src/main/java/ua/chekmaryov/barber_stat/BarberStatApplication.java
@@ -1,4 +1,4 @@
-package ua.chekmaryov.barber_stat.config;
+package ua.chekmaryov.barber_stat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
