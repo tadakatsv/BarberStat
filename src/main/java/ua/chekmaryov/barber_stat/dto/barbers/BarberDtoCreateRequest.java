11edffcec681f26499e7017f697a8437bbe38fe1
pain <141949752+tadakatsv@users.noreply.github.com>
2026-05-17 04:33:11 +0300

    Added validation to salaryPercent

--- src/main/java/ua/chekmaryov/barber_stat/dto/barbers/BarberDtoCreateRequest.java
+++ src/main/java/ua/chekmaryov/barber_stat/dto/barbers/BarberDtoCreateRequest.java
@@ -1,6 +1,8 @@
 package ua.chekmaryov.barber_stat.dto.barbers;
 
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
@@ -16,7 +18,9 @@ public record BarberDtoCreateRequest(
     @NotBlank(message = "Phone number is required") String phone,
     @NotNull(message = "Birth date is required") LocalDate birthDate,
     BarberStatus status,
-    @NotNull(message = "Role is required") BarberRole role,
+    BarberRole role,
+    @Min(value = 0, message = "Salary percent cannot be less than 0")
+    @Max(value = 100, message = "Salary percent cannot be more than 100")
     Integer salaryPercent,
     String notes
     ){
