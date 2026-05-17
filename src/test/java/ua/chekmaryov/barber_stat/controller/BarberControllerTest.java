484f7c5442f1977a4ccbf02d7e9385eeec13053f
pain <141949752+tadakatsv@users.noreply.github.com>
2026-05-17 04:36:29 +0300

    Made controller test for barbers

---
+++ src/test/java/ua/chekmaryov/barber_stat/controller/BarberControllerTest.java
@@ -0,0 +1,300 @@
+package ua.chekmaryov.barber_stat.controller;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
+import ua.chekmaryov.barber_stat.dto.barbers.BarberDtoCreateRequest;
+import ua.chekmaryov.barber_stat.dto.barbers.BarberDtoResponse;
+import ua.chekmaryov.barber_stat.dto.barbers.BarberDtoUpdateRequest;
+import ua.chekmaryov.barber_stat.enums.BarberRole;
+import ua.chekmaryov.barber_stat.enums.BarberStatus;
+import ua.chekmaryov.barber_stat.exception.AlreadyExistsException;
+import ua.chekmaryov.barber_stat.exception.ResourceNotFoundException;
+import ua.chekmaryov.barber_stat.service.BarberServiceImpl;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc()
+@WebMvcTest(BarberController.class)
+public class BarberControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @MockitoBean
+    private BarberServiceImpl barberService;
+
+    @Test
+    public void shouldCreateBarber() throws Exception{
+        BarberDtoCreateRequest request = BarberDtoCreateRequest.builder()
+                .firstName("Артур")
+                .lastName("Морган")
+                .phone("380666666666")
+                .birthDate(LocalDate.of(1868, Month.JUNE,22))
+                .status(null)
+                .role(BarberRole.TOP)
+                .salaryPercent(50)
+                .notes(null)
+                .build();
+
+        BarberDtoResponse response =
+                BarberDtoResponse.builder()
+                        .id(1L)
+                        .fullName("Артур Морган")
+                        .phone("380666666666")
+                        .birthDate(LocalDate.of(1868, Month.JUNE,22))
+                        .status(BarberStatus.ACTIVE)
+                        .role(BarberRole.TOP)
+                        .salaryPercent(50)
+                        .notes(null)
+                        .build();
+
+        when(barberService.create(request))
+                .thenReturn(response);
+
+
+        String json = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(post("/api/v1/barbers")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.fullName").value("Артур Морган"))
+                .andExpect(jsonPath("$.id").value(1L));
+
+        verify(barberService).create(Mockito.any(BarberDtoCreateRequest.class));
+    }
+
+    @Test
+    public void shouldReturn400_WhenFirstNameIsBlank() throws Exception {
+        // Arrange: створюємо некоректний запит (ім'я порожнє)
+        BarberDtoCreateRequest invalidRequest = BarberDtoCreateRequest.builder()
+                .firstName("") // Порожнє ім'я
+                .lastName("Морган")
+                .phone("380666666666")
+                .build();
+
+        String json = objectMapper.writeValueAsString(invalidRequest);
+        // Act & Assert
+        mockMvc.perform(post("/api/v1/barbers")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest()); // Чекаємо 400 помилку
+
+        // Перевіряємо, що сервіс навіть не викликався, бо валідація спрацювала раніше
+        verify(barberService, times(0)).create(any());
+    }
+
+    @Test
+    public void shouldGetException_WhenBarberByPhoneAlreadyExists() throws Exception{
+        BarberDtoCreateRequest request = BarberDtoCreateRequest.builder()
+                .firstName("Артур")
+                .lastName("Морган")
+                .phone("380666666666")
+                .birthDate(LocalDate.of(1868, Month.JUNE,22))
+                .status(null)
+                .role(BarberRole.TOP)
+                .salaryPercent(50)
+                .notes(null)
+                .build();
+
+        when(barberService.create(request))
+                .thenThrow(new AlreadyExistsException("Barber with " + request.phone() +" already exists"));
+
+        String json = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(post("/api/v1/barbers")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isConflict()); // Чекаємо 400 помилку
+    }
+
+    @Test
+    public void shouldReturnAllBarbers() throws Exception{
+        BarberDtoResponse response =
+                BarberDtoResponse.builder()
+                        .id(1L)
+                        .fullName("Артур Морган")
+                        .phone("380666666666")
+                        .birthDate(LocalDate.of(1868, Month.JUNE,22))
+                        .status(BarberStatus.ACTIVE)
+                        .role(BarberRole.TOP)
+                        .salaryPercent(50)
+                        .notes(null)
+                        .build();
+        Page<BarberDtoResponse> barberPage = new PageImpl<>(List.of(response));
+
+        when(barberService.getAll(any(Pageable.class))).thenReturn(barberPage);
+
+        mockMvc.perform(get("/api/v1/barbers"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content[0].fullName").value("Артур Морган"));
+
+        verify(barberService).getAll(any(Pageable.class));
+    }
+
+    @Test
+    public void shouldGetBarberById() throws Exception{
+        Long id = 1L;
+        BarberDtoResponse response =
+                BarberDtoResponse.builder()
+                        .id(1L)
+                        .fullName("Артур Морган")
+                        .phone("380666666666")
+                        .birthDate(LocalDate.of(1868, Month.JUNE,22))
+                        .status(BarberStatus.ACTIVE)
+                        .role(BarberRole.TOP)
+                        .salaryPercent(50)
+                        .notes(null)
+                        .build();
+
+        when(barberService.getById(id)).thenReturn(response);
+
+        mockMvc.perform(get("/api/v1/barbers/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(id));
+        verify(barberService).getById(anyLong());
+    }
+
+    @Test
+    public void shouldGetException_WhenNoBarberById() throws Exception{
+        Long id = 1L;
+
+        when(barberService.getById(id))
+                .thenThrow(new ResourceNotFoundException("Barber not found with id: " + id));
+
+        mockMvc.perform(get("/api/v1/barbers/1"))
+                .andExpect(status().isNotFound());
+        verify(barberService).getById(anyLong());
+    }
+
+    @Test
+    public void shouldUpdateById() throws Exception{
+        Long id = 1L;
+        BarberDtoUpdateRequest request = BarberDtoUpdateRequest.builder()
+                .firstName("Артур")
+                .lastName("Морган")
+                .phone("380666666666")
+                .birthDate(LocalDate.of(1868, Month.JUNE,22))
+                .status(null)
+                .role(BarberRole.TOP)
+                .salaryPercent(50)
+                .notes(null)
+                .build();
+
+        BarberDtoResponse response =
+                BarberDtoResponse.builder()
+                        .id(1L)
+                        .fullName("Артур Морган")
+                        .phone("380666666666")
+                        .birthDate(LocalDate.of(1868, Month.JUNE,22))
+                        .status(BarberStatus.ACTIVE)
+                        .role(BarberRole.TOP)
+                        .salaryPercent(50)
+                        .notes(null)
+                        .build();
+
+        String json = objectMapper.writeValueAsString(request);
+
+
+        when(barberService.updateById(id,request)).thenReturn(response);
+
+
+        mockMvc.perform(put("/api/v1/barbers/1")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(id));
+
+        verify(barberService).updateById(anyLong(),eq(request));
+    }
+
+    @Test
+    public void shouldSoftDeleteById() throws Exception{
+        Long id = 1L;
+        BarberDtoResponse response =
+                BarberDtoResponse.builder()
+                        .id(1L)
+                        .fullName("Артур Морган")
+                        .phone("380666666666")
+                        .birthDate(LocalDate.of(1868, Month.JUNE,22))
+                        .status(BarberStatus.FIRED)
+                        .role(BarberRole.TOP)
+                        .salaryPercent(50)
+                        .notes(null)
+                        .build();
+
+        when(barberService.deleteById(id)).thenReturn(response);
+
+        mockMvc.perform(delete("/api/v1/barbers/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value(BarberStatus.FIRED.name()));
+
+        verify(barberService).deleteById(anyLong());
+    }
+
+    @Test
+    public void shouldReturnBarbersByFirstName() throws Exception{
+        BarberDtoResponse response =
+                BarberDtoResponse.builder()
+                        .id(1L)
+                        .fullName("Артур Морган")
+                        .phone("380666666666")
+                        .birthDate(LocalDate.of(1868, Month.JUNE,22))
+                        .status(BarberStatus.ACTIVE)
+                        .role(BarberRole.TOP)
+                        .salaryPercent(50)
+                        .notes(null)
+                        .build();
+        Page<BarberDtoResponse> barberPage = new PageImpl<>(List.of(response));
+
+        when(barberService.findByFirstNameAndLastName(eq("Артур"),eq("Морган"), any(Pageable.class))).thenReturn(barberPage);
+
+        mockMvc.perform(get("/api/v1/barbers/search/by-first-name-and-second-name?firstName=Артур&lastName=Морган"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content[0].fullName").value("Артур Морган"));
+
+        verify(barberService).findByFirstNameAndLastName(anyString(),anyString(),any(Pageable.class));
+    }
+
+    @Test
+    public void shouldReturnBarbersByStatus() throws Exception{
+        BarberDtoResponse response =
+                BarberDtoResponse.builder()
+                        .id(1L)
+                        .fullName("Артур Морган")
+                        .phone("380666666666")
+                        .birthDate(LocalDate.of(1868, Month.JUNE,22))
+                        .status(BarberStatus.ACTIVE)
+                        .role(BarberRole.TOP)
+                        .salaryPercent(50)
+                        .notes(null)
+                        .build();
+        Page<BarberDtoResponse> barberPage = new PageImpl<>(List.of(response));
+
+        when(barberService.findByStatus(eq(BarberStatus.ACTIVE), any(Pageable.class))).thenReturn(barberPage);
+
+        mockMvc.perform(get("/api/v1/barbers/search/by-status?status=ACTIVE"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content[0].status").value(BarberStatus.ACTIVE.name()));
+
+        verify(barberService).findByStatus(eq(BarberStatus.ACTIVE), any(Pageable.class));
+    }
+}
