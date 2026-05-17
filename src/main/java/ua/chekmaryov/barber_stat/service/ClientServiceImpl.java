7e64678469af265154dec6c1319f1c4d7d951dcb
pain <141949752+tadakatsv@users.noreply.github.com>
2026-05-17 04:28:35 +0300

    Made all needed methods

--- src/main/java/ua/chekmaryov/barber_stat/service/ClientServiceImpl.java
+++ src/main/java/ua/chekmaryov/barber_stat/service/ClientServiceImpl.java
@@ -9,18 +9,21 @@ import ua.chekmaryov.barber_stat.dto.clients.ClientDtoCreateRequest;
 import ua.chekmaryov.barber_stat.dto.clients.ClientDtoResponse;
 import ua.chekmaryov.barber_stat.dto.clients.ClientDtoUpdateRequest;
 import ua.chekmaryov.barber_stat.entity.Client;
+import ua.chekmaryov.barber_stat.enums.ClientStatus;
 import ua.chekmaryov.barber_stat.exception.AlreadyExistsException;
+import ua.chekmaryov.barber_stat.exception.BadRequestException;
+import ua.chekmaryov.barber_stat.exception.ResourceNotFoundException;
 import ua.chekmaryov.barber_stat.mapper.ClientMapper;
 import ua.chekmaryov.barber_stat.repository.ClientRepository;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Slf4j
 @Service
 public class ClientServiceImpl implements ClientService{
     private final ClientRepository clientRepository;
     private final ClientMapper clientMapper;
-
 
     public ClientServiceImpl(ClientRepository clientRepository, ClientMapper clientMapper) {
         this.clientRepository = clientRepository;
@@ -32,7 +35,7 @@ public class ClientServiceImpl implements ClientService{
     @Transactional
     public ClientDtoResponse create(ClientDtoCreateRequest request) {
         log.info("Request to create a new Client: {} {}", request.firstName(),request.lastName());
-        if(clientRepository.existsByPhone(request.phone())){
+        if(clientRepository.existsByPhone(request.phone().replaceAll("\\s+",""))){
             throw new AlreadyExistsException("Client with " + request.phone() +" already exists");
         }
         Client client = clientRepository.save(clientMapper.dtoToEntity(request));
@@ -42,42 +45,79 @@ public class ClientServiceImpl implements ClientService{
     @Override
     @Transactional
     public Page<ClientDtoResponse> getAll(Pageable pageable) {
-        return null;
+        log.info("Request to fetch all clients");
+        Page<Client> allClients = clientRepository.findAll(pageable);
+        log.debug("Retrieved {} records from database",allClients.getTotalElements());
+        return allClients.map(clientMapper::toResponse);
     }
 
     @Override
     @Transactional
     public ClientDtoResponse getById(Long id) {
-        return null;
+        log.info("Searching for client with id: {}" ,id);
+        Client client = clientRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Client not found with id: " + id));
+        log.debug("Successfully found client: {} (ID: {})", client.getLastName(), id);
+        return clientMapper.toResponse(client);
     }
 
     @Override
     @Transactional
     public ClientDtoResponse updateById(Long id, ClientDtoUpdateRequest request) {
-        return null;
+        log.info("Updating client with ID: {}",id);
+        Client client = clientRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Client not found with id: " + id));
+        if (!Objects.equals(client.getPhone(), request.phone().replaceAll("\\s+",""))){
+            if(clientRepository.existsByPhone(request.phone().replaceAll("\\s+",""))){
+                throw new AlreadyExistsException("Client with " + request.phone() +" already exists");
+            }
+        }
+        Client updated = clientRepository.save(clientMapper.dtoUpdateToEntity(request,client));
+        log.debug("Client ID {} successfully updated", id);
+        return clientMapper.toResponse(updated);
     }
 
     @Override
     @Transactional
     public ClientDtoResponse deleteById(Long id) {
-        return null;
+        log.info("Attempting to soft delete client with ID: {}", id);
+        Client client = clientRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Client not found with id: " + id));
+        log.debug("Client with ID: {} was found ",id);
+        client.setStatus(ClientStatus.ARCHIVED);
+        return clientMapper.toResponse(client);
     }
 
     @Override
     @Transactional
     public ClientDtoResponse getByPhone(String phone) {
-        return null;
+        log.info("Searching for a client by a phone: {}", phone);
+        Client client = clientRepository.findClientByPhone(phone)
+                .orElseThrow(() -> new ResourceNotFoundException("Client not found with phone: " + phone));
+        log.debug("Client was found with a phone: {} ", phone);
+        return clientMapper.toResponse(client);
     }
 
     @Override
     @Transactional
     public Page<ClientDtoResponse> findByFirstNameAndLastName(String firstName, String lastName, Pageable pageable) {
-        return null;
+        log.info("Searching for clients with filter: firstName={}, lastName={}", firstName, lastName);
+        Page<ClientDtoResponse> clients = clientRepository.findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(firstName,lastName, pageable)
+                .map(clientMapper::toResponse);
+        log.debug("Clients was found with firstname {} and last name {}",firstName,lastName);
+        return clients;
     }
 
     @Override
     @Transactional
     public Page<ClientDtoResponse> findByLastVisitDateBetween(LocalDate lastVisitDateAfter, LocalDate lastVisitDateBefore, Pageable pageable) {
-        return null;
+        log.info("Attempting to find client between {} and {}", lastVisitDateAfter,lastVisitDateBefore);
+        if (lastVisitDateAfter.isAfter(lastVisitDateBefore)){
+            throw new BadRequestException("Start date (" + lastVisitDateAfter + ") cannot be after end date (" + lastVisitDateBefore + ")");
+        }
+        Page<ClientDtoResponse> clients = clientRepository.findClientsByLastVisitDateBetween(lastVisitDateAfter,lastVisitDateBefore,pageable)
+                .map(clientMapper::toResponse);
+        log.debug("Search complete. Found {} clients matching the criteria", clients.getTotalElements());
+        return clients;
     }
 }
