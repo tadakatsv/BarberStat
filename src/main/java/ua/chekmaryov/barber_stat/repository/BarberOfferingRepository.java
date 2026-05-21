a3a2c1507da7a94928efe6f02bf2e119d01db5fc
pain <141949752+tadakatsv@users.noreply.github.com>
2026-05-21 05:09:26 +0300

    Add check barberOffering By barber_id and offer_id

--- src/main/java/ua/chekmaryov/barber_stat/repository/BarberOfferingRepository.java
+++ src/main/java/ua/chekmaryov/barber_stat/repository/BarberOfferingRepository.java
@@ -3,12 +3,13 @@ package ua.chekmaryov.barber_stat.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.chekmaryov.barber_stat.entity.BarberOffering;
-import ua.chekmaryov.barber_stat.entity.Offer;
 
-import java.math.BigDecimal;
 import java.util.Optional;
 
 @Repository
 public interface BarberOfferingRepository extends JpaRepository<BarberOffering,Long> {
     Optional<BarberOffering> findByBarberIdAndOfferId(Long barberId, Long offerId);
+
+    //для проверки перед тем как добавлять в таблицу
+    boolean existsByBarber_IdAndOffer_Id(Long barberId, Long offerId);
 }
