976f16c7e0980f348453c0b9063135fda1a16f84
pain <141949752+tadakatsv@users.noreply.github.com>
2026-05-21 04:59:28 +0300

    Add Paging to OfferRepository

--- src/main/java/ua/chekmaryov/barber_stat/repository/OfferRepository.java
+++ src/main/java/ua/chekmaryov/barber_stat/repository/OfferRepository.java
@@ -1,13 +1,12 @@
 package ua.chekmaryov.barber_stat.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.chekmaryov.barber_stat.entity.Offer;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface OfferRepository extends JpaRepository<Offer,Long> {
-    List<Offer> findByNameContainingIgnoreCase(String name);
+    Page<Offer> findByNameContainingIgnoreCase(String name, Pageable pageable);
 }
