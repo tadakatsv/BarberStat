250b5c90afeffe68e1a6ba3e3fee739e19983795
pain <141949752+tadakatsv@users.noreply.github.com>
2026-05-30 17:52:48 +0300

    Add paging to VisitRepository

--- src/main/java/ua/chekmaryov/barber_stat/repository/VisitRepository.java
+++ src/main/java/ua/chekmaryov/barber_stat/repository/VisitRepository.java
@@ -1,19 +1,36 @@
 package ua.chekmaryov.barber_stat.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ua.chekmaryov.barber_stat.entity.Visit;
+import ua.chekmaryov.barber_stat.enums.VisitStatus;
 
-import java.time.Instant;
-import java.util.List;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Repository
 public interface VisitRepository extends JpaRepository<Visit, Long> {
-    List<Visit> findVisitByClientId(Long clientId);
+    Page<Visit> findVisitsByStatusAndVisitTimeBetween(VisitStatus status, LocalDateTime visitTimeAfter, LocalDateTime visitTimeBefore, Pageable pageable);
 
-    List<Visit> findByClientIdAndVisitTimeBetween(Long clientId, Instant start, Instant end);
+    Page<Visit> findVisitsByClient_IdAndStatus(Long clientId, VisitStatus status, Pageable pageable);
 
-    List<Visit> findByVisitTimeBetween(Instant start, Instant end);
+    Page<Visit> findByClientIdAndVisitTimeBetween(Long clientId, LocalDateTime start, LocalDateTime end, Pageable pageable);
+
+    Page<Visit> findByVisitTimeBetween(LocalDateTime start, LocalDateTime end,Pageable pageable);
+
+    Page<Visit> findByBarberIdAndVisitTimeBetween(Long barberId, LocalDateTime start, LocalDateTime end,Pageable pageable);
+
+    //сделать ещё чтоб нельзя было на одно время с одним статусом
+//    boolean existsByBarberIdAndVisitTime(Long barberId, LocalDateTime visitTime);
+//
+//    boolean existsByBarber_IdAndClient_IdAndStatus(Long barberId, Long clientId, VisitStatus status);
+
+    //boolean existsByVisitTimeBetweenAndBarber_IdAndStatus(LocalDateTime visitTimeAfter, LocalDateTime visitTimeBefore, Long barberId, VisitStatus status);
 
     List<Visit> findByBarberIdAndVisitTimeBetween(Long barberId, Instant start, Instant end);
 
