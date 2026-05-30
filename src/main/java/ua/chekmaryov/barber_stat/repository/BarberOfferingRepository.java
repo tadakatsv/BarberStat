f02923bc39fe360bf32459f1fa620b0896457d77
pain <141949752+tadakatsv@users.noreply.github.com>
2026-05-30 17:38:53 +0300

    Add findBarberOfferingsByBarber_Id and findBarberOfferingsByOffer_Id

--- src/main/java/ua/chekmaryov/barber_stat/repository/BarberOfferingRepository.java
+++ src/main/java/ua/chekmaryov/barber_stat/repository/BarberOfferingRepository.java
@@ -1,5 +1,7 @@
 package ua.chekmaryov.barber_stat.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.chekmaryov.barber_stat.entity.BarberOffering;
@@ -10,6 +12,11 @@ import java.util.Optional;
 public interface BarberOfferingRepository extends JpaRepository<BarberOffering,Long> {
     Optional<BarberOffering> findByBarberIdAndOfferId(Long barberId, Long offerId);
 
+    Page<BarberOffering> findBarberOfferingsByBarber_Id(Long barberId, Pageable pageable);
+
+    Page<BarberOffering> findBarberOfferingsByOffer_Id(Long offerId, Pageable pageable);
+
+    //делать ли в service эту тему
     //для проверки перед тем как добавлять в таблицу
     boolean existsByBarber_IdAndOffer_Id(Long barberId, Long offerId);
 }
