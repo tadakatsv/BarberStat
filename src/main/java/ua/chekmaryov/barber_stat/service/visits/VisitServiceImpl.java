fa20bf033dca3de3ba81437aa041260fb705a091
pain <141949752+tadakatsv@users.noreply.github.com>
2026-06-02 01:58:24 +0300

    Add logs to VisitServiceImpl

--- src/main/java/ua/chekmaryov/barber_stat/service/visits/VisitServiceImpl.java
+++ src/main/java/ua/chekmaryov/barber_stat/service/visits/VisitServiceImpl.java
@@ -77,7 +77,9 @@ public class VisitServiceImpl implements VisitService{
     @Override
     @Transactional
     public Page<VisitDtoResponse> getAllByStatusAndBetweenTwoDates(VisitStatus status, LocalDateTime visitTimeAfter, LocalDateTime visitTimeBefore, Pageable pageable) {
+        log.info("Attempt to find visits with status {} between {} and {}",status,visitTimeAfter,visitTimeBefore);
         Page<Visit> neededVisits = visitRepository.findVisitsByStatusAndVisitTimeBetween(status,visitTimeAfter,visitTimeBefore,pageable);
+        log.debug("Found {} visits with status {} between {} and {}", neededVisits.getNumberOfElements(),status,visitTimeAfter,visitTimeBefore);
         return neededVisits
                 .map(mapper::toResponse);
     }
@@ -85,16 +87,20 @@ public class VisitServiceImpl implements VisitService{
     @Override
     @Transactional
     public VisitDtoResponse getById(Long id) {
+        log.info("Attempt to find visit with id:{}",id);
         Visit visit = visitRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("No visit by that id"));
+        log.debug("Found visit with id {}",id);
         return mapper.toResponse(visit);
     }
 
     @Override
     @Transactional
     public VisitDtoResponse updateById(Long id, VisitDtoUpdateRequest request) {
+        log.info("Attempt to find visit with id:{}",id);
         Visit visit = visitRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("No visit by that id"));
+        log.debug("Found visit with id {}",id);
         LocalDateTime visitTimeStart;
         LocalDateTime visitTimeEnd;
         Integer duration;
@@ -112,27 +118,34 @@ public class VisitServiceImpl implements VisitService{
                 visitTimeEnd = visit.getVisitTime().plusMinutes(duration);
         }
         if (request.durationMinutes() !=null || request.visitTime() != null) {
+            log.debug("Check if new time is overlapping but for himself");
             if (visitRepository.hasOverlappingVisitButForHimself(visit.getBarber().getId(), visit.getId(), visitTimeStart, visitTimeEnd)) {
                     throw new AlreadyExistsException("Barber already booked on this time " + visitTimeStart);
             }
         }
+        log.info("Request to updated visit");
         Visit updated = visitRepository.save(mapper.dtoUpdateToEntity(request,visit));
+        log.debug("Visit with id {} successfully saved", updated.getId());
         return mapper.toResponse(updated);
     }
 
     @Override
     @Transactional
     public VisitDtoResponse cancelVisitById(Long id) {
+        log.debug("Attempting to cancel visit with ID: {}", id);
         Visit visit = visitRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("No visit by that id"));
         visit.setStatus(VisitStatus.CANCELLED);
+        log.info("Visit with ID: {} was successfully CANCELLED", id);
         return mapper.toResponse(visit);
     }
 
     @Override
     @Transactional
     public Page<VisitDtoResponse> findVisitByClient_IdAndStatus(Long clientId, VisitStatus status, Pageable pageable) {
+        log.debug("Searching visits for client ID: {} with status {}", clientId, status);
         Page<Visit> neededVisits = visitRepository.findVisitsByClient_IdAndStatus(clientId,status,pageable);
+        log.debug("Found {} visits for client ID: {}", neededVisits.getNumberOfElements(), clientId);
         return neededVisits
                 .map(mapper::toResponse);
     }
@@ -140,10 +153,12 @@ public class VisitServiceImpl implements VisitService{
     @Override
     @Transactional
     public Page<VisitDtoResponse> findByClientIdAndVisitTimeBetween(Long clientId, LocalDateTime start, LocalDateTime end, Pageable pageable) {
+        log.debug("Searching visits for client ID: {} between {} and {}", clientId, start, end);
         if (start.isAfter(end)){
             throw new BadRequestException("Start date (" + start + ") cannot be after end date (" + end + ")");
         }
         Page<Visit> neededVisits = visitRepository.findByClientIdAndVisitTimeBetween(clientId, start, end, pageable);
+        log.debug("Found {} visits for client ID: {}", neededVisits.getNumberOfElements(), clientId);
         return neededVisits
                 .map(mapper::toResponse);
     }
@@ -151,10 +166,12 @@ public class VisitServiceImpl implements VisitService{
     @Override
     @Transactional
     public Page<VisitDtoResponse> findByVisitTimeBetween(LocalDateTime start, LocalDateTime end, Pageable pageable) {
+        log.info("Request to find visits between {} and {}",start,end);
         if (start.isAfter(end)){
             throw new BadRequestException("Start date (" + start + ") cannot be after end date (" + end + ")");
         }
         Page<Visit> neededVisits = visitRepository.findByVisitTimeBetween(start, end, pageable);
+        log.debug("Found {} visits", neededVisits.getNumberOfElements());
         return neededVisits
                 .map(mapper::toResponse);
     }
@@ -162,10 +179,12 @@ public class VisitServiceImpl implements VisitService{
     @Override
     @Transactional
     public Page<VisitDtoResponse> findByBarberIdAndVisitTimeBetween(Long barberId, LocalDateTime start, LocalDateTime end, Pageable pageable) {
+        log.info("Attempt to find visits with barberID {} between {} and {}",barberId,start,end);
         if (start.isAfter(end)){
             throw new BadRequestException("Start date (" + start + ") cannot be after end date (" + end + ")");
         }
         Page<Visit> neededVisits = visitRepository.findByBarberIdAndVisitTimeBetween(barberId, start, end, pageable);
+        log.debug("Found {} needed visits for barber ID: {}",neededVisits.getTotalElements(),barberId);
         return neededVisits
                 .map(mapper::toResponse);
     }
