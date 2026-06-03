e40c79ed37065ac2eac46b7da2fc2d7766db0306
pain <141949752+tadakatsv@users.noreply.github.com>
2026-06-04 00:07:29 +0300

    Add validation @Past to lastVisitDate and regular expression pattern to phone

--- src/main/java/ua/chekmaryov/barber_stat/dto/clients/ClientDtoCreateRequest.java
+++ src/main/java/ua/chekmaryov/barber_stat/dto/clients/ClientDtoCreateRequest.java
@@ -1,7 +1,6 @@
 package ua.chekmaryov.barber_stat.dto.clients;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.Builder;
 import ua.chekmaryov.barber_stat.enums.ClientStatus;
 
@@ -9,11 +8,17 @@ import java.time.LocalDate;
 
 @Builder
 public record ClientDtoCreateRequest(
-        @NotBlank(message = "First name is required") String firstName,
-        @NotBlank(message = "Last name is required") String lastName,
-        @NotBlank(message = "Phone number is required") String phone,
-        @NotNull(message = "Birth date is required") LocalDate birthDate,
+        @NotBlank(message = "First name is required")
+        String firstName,
+        @NotBlank(message = "Last name is required")
+        String lastName,
+        @NotBlank(message = "Phone number is required")
+        @Pattern(regexp = "^\\d{9,12}$", message = "Invalid phone format")
+        String phone,
+        @NotNull(message = "Birth date is required")
+        LocalDate birthDate,
         ClientStatus status,
+        @Past(message = "Last visit date can't be in the past")
         LocalDate lastVisitDate,
         String notes
 ) {
