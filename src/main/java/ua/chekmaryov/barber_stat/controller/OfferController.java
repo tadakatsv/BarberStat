5fee53a8641d09345c2a4728484d7dab3bf8dec0
pain <141949752+tadakatsv@users.noreply.github.com>
2026-06-23 22:58:57 +0300

    Add search filter to get all

--- src/main/java/ua/chekmaryov/barber_stat/controller/OfferController.java
+++ src/main/java/ua/chekmaryov/barber_stat/controller/OfferController.java
@@ -1,63 +1,63 @@
-//package ua.chekmaryov.barber_stat.controller;
-//
-//import jakarta.validation.Valid;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springdoc.core.annotations.ParameterObject;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.Pageable;
-//import org.springframework.data.domain.Sort;
-//import org.springframework.data.web.PageableDefault;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.web.bind.annotation.*;
-//import ua.chekmaryov.barber_stat.dto.offers.OfferDtoRequest;
-//import ua.chekmaryov.barber_stat.dto.offers.OfferDtoResponse;
-//
-//@RestController
-//@Slf4j
-//@RequestMapping("/api/v1/offers")
-//public class OfferController {
-//
-//    private final OfferService offerService;
-//
-//    public OfferController(OfferService offerService) {
-//        this.offerService = offerService;
-//    }
-//
-//    @PostMapping
-//    @ResponseStatus(HttpStatus.CREATED)
-//    public OfferDtoResponse createOffer(
-//            @Valid @RequestBody OfferDtoRequest request
-//            ){
-//        return offerService.create(request);
-//    }
-//
-//    @GetMapping
-//    public Page<OfferDtoResponse> getAllOffers(
-//            @ParameterObject @PageableDefault(size = 10, sort = "name", direction = Sort.Direction.ASC) Pageable pageable){
-//        return offerService.getAll(pageable);
-//    }
-//
-//    @GetMapping("/{id}")
-//    public OfferDtoResponse getOfferById(
-//            @PathVariable("id") Long id
-//    ){
-//        return offerService.getById(id);
-//    }
-//
-//    @PutMapping("/{id}")
-//    public OfferDtoResponse updateOfferById(
-//            @PathVariable("id") Long id,
-//            @Valid @RequestBody OfferDtoRequest request
-//    ){
-//        return offerService.updateById(id,request);
-//    }
-//
-//    @GetMapping("/search/by-name")
-//    public Page<OfferDtoResponse> findByName(
-//            @ParameterObject @PageableDefault(size = 10, sort = "name", direction = Sort.Direction.ASC) Pageable pageable,
-//            @RequestParam("name") String name
-//    ){
-//        return offerService.findByName(name, pageable);
-//    }
-//
-//}
+package ua.chekmaryov.barber_stat.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ua.chekmaryov.barber_stat.dto.offers.*;
+import ua.chekmaryov.barber_stat.service.offers.OfferFacade;
+
+@RestController
+@Slf4j
+@RequestMapping("/api/v1/offers")
+@RequiredArgsConstructor
+
+public class OfferController {
+
+    private final OfferFacade offerFacade;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public OfferDtoResponse createOffer(
+            @Valid @RequestBody OfferDtoPostRequest request
+    ) {
+        return offerFacade.create(request);
+    }
+
+    @GetMapping
+    public Page<OfferDtoResponse> getAllOffers(
+            @ParameterObject @PageableDefault(size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
+            @ModelAttribute OfferSearchFilters offerSearchFilters) {
+        return offerFacade.getAll(pageable, offerSearchFilters);
+    }
+
+    @GetMapping("/{id}")
+    public OfferDtoResponse getOfferById(
+            @PathVariable("id") Long id
+    ) {
+        return offerFacade.getById(id);
+    }
+
+    @PutMapping("/{id}")
+    public OfferDtoResponse updateOfferById(
+            @PathVariable("id") Long id,
+            @Valid @RequestBody OfferDtoPutRequest request
+    ) {
+        return offerFacade.updateById(id, request);
+    }
+
+    @PatchMapping("/{id}")
+    public OfferDtoResponse patchOfferById(
+            @PathVariable("id") Long id,
+            @Valid @RequestBody OfferDtoPatchRequest request
+    ) {
+        return offerFacade.patchById(id, request);
+    }
+
+}
--- src/main/java/ua/chekmaryov/barber_stat/controller/VisitsController.java
+++ src/main/java/ua/chekmaryov/barber_stat/controller/VisitsController.java
@@ -1,126 +1,64 @@
-//package ua.chekmaryov.barber_stat.controller;
-//
-//import jakarta.validation.Valid;
-//import jakarta.validation.constraints.Min;
-//import jakarta.validation.constraints.Positive;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springdoc.core.annotations.ParameterObject;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.Pageable;
-//import org.springframework.data.domain.Sort;
-//import org.springframework.data.web.PageableDefault;
-//import org.springframework.format.annotation.DateTimeFormat;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.web.bind.annotation.DeleteMapping;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.PutMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.ResponseStatus;
-//import org.springframework.web.bind.annotation.RestController;
-//import ua.chekmaryov.barber_stat.dto.visits.VisitDtoCreateRequest;
-//import ua.chekmaryov.barber_stat.dto.visits.VisitDtoResponse;
-//import ua.chekmaryov.barber_stat.dto.visits.VisitDtoUpdateRequest;
-//import ua.chekmaryov.barber_stat.enums.VisitStatus;
-//import ua.chekmaryov.barber_stat.service.visits.VisitService;
-//
-//import java.time.LocalDate;
-//import java.time.LocalDateTime;
-//import java.time.LocalTime;
-//
-//@RestController
-//@Slf4j
-//@RequestMapping("/api/v1/visits")
-//public class VisitsController {
-//
-//    private final VisitService service;
-//
-//    public VisitsController(VisitService service) {
-//        this.service = service;
-//    }
-//
-//    @PostMapping
-//    @ResponseStatus(HttpStatus.CREATED)
-//    public VisitDtoResponse createVisit(
-//            @Valid @RequestBody VisitDtoCreateRequest request
-//            ){
-//        return service.create(request);
-//    }
-//
-//    @GetMapping//я хочу чтоб не было getAll, ибо не имеет смысла. Хотя тут Page может просто, чтоб сортировало с конца. Не знаю как именно нагружается база
-//    public Page<VisitDtoResponse> getAll(
-//            @RequestParam(value = "status", required = false, defaultValue = "PLANNED") VisitStatus status,
-//            @RequestParam(value = "start", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime start,
-//            @RequestParam(value = "end", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime end,
-//            @ParameterObject @PageableDefault(size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable
-//    ) {
-//        if (start == null) {
-//            start = LocalDate.now().atStartOfDay();
-//        }
-//        if (end == null) {
-//            end = LocalDate.now().atTime(LocalTime.MAX);
-//        }
-//
-//        return service.getAllByStatusAndBetweenTwoDates(status, start, end, pageable);
-//    }
-//
-//    @GetMapping("/{id}")
-//    public VisitDtoResponse getById(
-//            @PathVariable("id") @Positive Long id
-//    ){
-//        return service.getById(id);
-//    }
-//
-//    @PutMapping("/{id}")
-//    public VisitDtoResponse updateBarberById(
-//            @PathVariable("id") @Positive Long id,
-//            @RequestBody @Valid VisitDtoUpdateRequest request){
-//        return service.updateById(id,request);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public VisitDtoResponse cancelById(
-//            @PathVariable("id") @Positive Long id
-//    ){
-//        return service.cancelVisitById(id);
-//    }
-//
-//    @GetMapping("/search/by-client_id-and-status")
-//    public Page<VisitDtoResponse> findVisitByClient_IdAndStatus(
-//            @RequestParam(name = "client_id") @Positive Long clientId,
-//            @RequestParam(name = "status") VisitStatus status,
-//            @ParameterObject @PageableDefault(size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
-//        return service.findVisitsByClient_IdAndStatus(clientId, status, pageable);
-//    }
-//
-//    @GetMapping("/search/by-client_id-and-visit-time-between")
-//    public Page<VisitDtoResponse> findByClientIdAndVisitTimeBetween(
-//            @RequestParam(name = "client_id") @Positive  Long clientId,
-//            @RequestParam(value = "start", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime start,
-//            @RequestParam(value = "end", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime end,
-//            @ParameterObject @PageableDefault(size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
-//        return service.findByClientIdAndVisitTimeBetween(clientId, start, end, pageable);
-//    }
-//
-//    @GetMapping("/search/by-visit-time-between")
-//    public Page<VisitDtoResponse> findByVisitTimeBetween(
-//            @RequestParam(value = "start", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime start,
-//            @RequestParam(value = "end", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime end,
-//            @ParameterObject @PageableDefault(size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
-//        return service.findByVisitTimeBetween(start, end, pageable);
-//    }
-//
-//    @GetMapping("/search/by-barber-id-and-visit-time-between")
-//    public Page<VisitDtoResponse> findByBarberIdAndVisitTimeBetween(
-//            @RequestParam(name = "barber_id") @Min(0) Long barberId,
-//            @RequestParam(value = "start", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime start,
-//            @RequestParam(value = "end", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime end,
-//            @ParameterObject @PageableDefault(size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
-//        return service.findByBarberIdAndVisitTimeBetween(barberId, start, end, pageable);
-//    }
-//
-//
-//}
+package ua.chekmaryov.barber_stat.controller;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ua.chekmaryov.barber_stat.dto.visits.*;
+import ua.chekmaryov.barber_stat.service.visits.VisitFacade;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/visits")
+public class VisitsController {
+
+    private final VisitFacade visitFacade;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public VisitDtoResponse createVisit(
+            @Valid @RequestBody VisitDtoPostRequest request
+    ) {
+        return visitFacade.create(request);
+    }
+
+    @GetMapping
+    public Page<VisitDtoResponse> getAll(
+            @ParameterObject @PageableDefault(size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
+            @ModelAttribute VisitSearchFilters visitSearchFilters) {
+
+        return visitFacade.getAll(pageable, visitSearchFilters);
+    }
+
+    @GetMapping("/{id}")
+    public VisitDtoResponse getById(
+            @PathVariable("id") @Positive Long id
+    ) {
+        return visitFacade.getById(id);
+    }
+
+    @PutMapping("/{id}")
+    public VisitDtoResponse updateById(
+            @PathVariable("id") @Positive Long id,
+            @RequestBody @Valid VisitDtoPutRequest request) {
+        return visitFacade.updateById(id, request);
+    }
+
+    @PatchMapping("/{id}")
+    public VisitDtoResponse patchById(
+            @PathVariable("id") @Positive Long id,
+            @RequestBody @Valid VisitDtoPatchRequest request
+    ) {
+        return visitFacade.patchById(id, request);
+    }
+
+
+}
