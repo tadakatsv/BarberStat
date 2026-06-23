5bf302ff0b67385824879b2bffe9691956e5b7ad
pain <141949752+tadakatsv@users.noreply.github.com>
2026-06-23 22:50:25 +0300

    Experimental findAll

--- src/main/java/ua/chekmaryov/barber_stat/repository/BarberRepository.java
+++ src/main/java/ua/chekmaryov/barber_stat/repository/BarberRepository.java
@@ -3,13 +3,16 @@ package ua.chekmaryov.barber_stat.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import ua.chekmaryov.barber_stat.dto.BarberSearchFilters;
 import ua.chekmaryov.barber_stat.entity.Barber;
+import ua.chekmaryov.barber_stat.enums.BarberRole;
+import ua.chekmaryov.barber_stat.enums.BarberStatus;
 
 @Repository
-public interface BarberRepository extends JpaRepository<Barber,Long> {
+public interface BarberRepository extends JpaRepository<Barber, Long>, JpaSpecificationExecutor<Barber> {
 
     /*
      * how it should be like
@@ -21,10 +24,20 @@ public interface BarberRepository extends JpaRepository<Barber,Long> {
             AND (:last_name is null or b.last_name = :last_name)
      */
     @Query(value = """
-
+            SELECT b FROM Barber b
+                   WHERE (:barberStatus is null or b.status = :barberStatus)
+                        AND (:barberRole is null or b.role = :barberRole)
+                        AND (:firstName is null or b.firstName = :firstName)
+                        AND (:lastName is null or b.lastName = :lastName)
+            
             """
     )
-    Page<Barber> findAll(Pageable pageable, BarberSearchFilters barberSearchFilters);
+    Page<Barber> findAll(Pageable pageable,
+                         @Param("firstName") String firstName,
+                         @Param("lastName") String lastName,
+                         @Param("barberStatus") BarberStatus barberStatus,
+                         @Param("barberRole") BarberRole barberRole
+    );
 
     boolean existsByPhone(String phone);
 }
