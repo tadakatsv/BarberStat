356e604b2ba885b853e3c914b5b2e39b6a14f1a6
pain <141949752+tadakatsv@users.noreply.github.com>
2026-06-23 22:43:10 +0300

    Add mapper dto to entity for put

--- src/main/java/ua/chekmaryov/barber_stat/mapper/BarberMapper.java
+++ src/main/java/ua/chekmaryov/barber_stat/mapper/BarberMapper.java
@@ -1,41 +1,63 @@
 package ua.chekmaryov.barber_stat.mapper;
 
 import org.springframework.stereotype.Component;
-import ua.chekmaryov.barber_stat.dto.barbers.BarberDtoCreateRequest;
+import ua.chekmaryov.barber_stat.dto.barbers.BarberDtoPostRequest;
+import ua.chekmaryov.barber_stat.dto.barbers.BarberDtoPutRequest;
 import ua.chekmaryov.barber_stat.dto.barbers.BarberDtoResponse;
-import ua.chekmaryov.barber_stat.dto.barbers.BarberDtoUpdateRequest;
+import ua.chekmaryov.barber_stat.dto.barbers.BarberDtoPatchRequest;
 import ua.chekmaryov.barber_stat.entity.Barber;
 
 @Component
 public class BarberMapper {
 
-    public Barber dtoToEntity(BarberDtoCreateRequest request){
+    public Barber dtoToEntity(BarberDtoPostRequest request) {
         return updateEntityFromDto(request, new Barber());
     }
 
-    public Barber dtoUpdateToEntity(BarberDtoUpdateRequest request, Barber toUpdate) {
+    public Barber dtoToEntity(BarberDtoPatchRequest request, Barber toUpdate) {
         return updateEntityFromDto(request, toUpdate);
     }
 
-    private Barber updateEntityFromDto(BarberDtoCreateRequest request, Barber toUpdate) {
+    public Barber dtoToEntity(Long id, BarberDtoPutRequest request) {
+        Barber barber = new Barber();
+        barber.setId(id);
+        return updateEntityFromDto(request, barber);
+    }
+
+    private Barber updateEntityFromDto(BarberDtoPostRequest request, Barber toUpdate) {
         toUpdate.setFirstName(request.firstName().trim());
         toUpdate.setLastName(request.lastName().trim());
-        toUpdate.setPhone(request.phone().replaceAll("\\s+",""));
+        toUpdate.setPhone(request.phone().replaceAll("\\s+", ""));
         toUpdate.setBirthDate(request.birthDate());
         if (request.status() != null) toUpdate.setStatus(request.status());
         if (request.role() != null) toUpdate.setRole(request.role());
-        if (request.salaryPercent() != null)toUpdate.setSalaryPercent(request.salaryPercent());
+        if (request.salaryPercent() != null) toUpdate.setSalaryPercent(request.salaryPercent());
         toUpdate.setNotes(request.notes());
         return toUpdate;
     }
 
-    private Barber updateEntityFromDto(BarberDtoUpdateRequest request, Barber toUpdate) {
-        if (request.firstName() != null && !request.firstName().isBlank()) toUpdate.setFirstName(request.firstName().trim());
-        if (request.lastName() != null && !request.lastName().isBlank()) toUpdate.setLastName(request.lastName().trim());
-        if (request.phone() != null && !request.phone().isBlank()) toUpdate.setPhone(request.phone().replaceAll("\\s+",""));
+    private Barber updateEntityFromDto(BarberDtoPutRequest request, Barber toUpdate) {
+        toUpdate.setFirstName(request.firstName().trim());
+        toUpdate.setLastName(request.lastName().trim());
+        toUpdate.setPhone(request.phone().replaceAll("\\s+", ""));
+        toUpdate.setBirthDate(request.birthDate());
+        toUpdate.setStatus(request.status());
+        toUpdate.setRole(request.role());
+        toUpdate.setSalaryPercent(request.salaryPercent());
+        toUpdate.setNotes(request.notes());
+        return toUpdate;
+    }
+
+    private Barber updateEntityFromDto(BarberDtoPatchRequest request, Barber toUpdate) {
+        if (request.firstName() != null && !request.firstName().isBlank())
+            toUpdate.setFirstName(request.firstName().trim());
+        if (request.lastName() != null && !request.lastName().isBlank())
+            toUpdate.setLastName(request.lastName().trim());
+        if (request.phone() != null && !request.phone().isBlank())
+            toUpdate.setPhone(request.phone().replaceAll("\\s+", ""));
         if (request.birthDate() != null) toUpdate.setBirthDate(request.birthDate());
-        if (request.status() != null ) toUpdate.setStatus(request.status());
-        if (request.role() != null ) toUpdate.setRole(request.role());
+        if (request.status() != null) toUpdate.setStatus(request.status());
+        if (request.role() != null) toUpdate.setRole(request.role());
         if (request.salaryPercent() != null) toUpdate.setSalaryPercent(request.salaryPercent());
         if (request.notes() != null && !request.notes().isBlank()) toUpdate.setNotes(request.notes());
         return toUpdate;
