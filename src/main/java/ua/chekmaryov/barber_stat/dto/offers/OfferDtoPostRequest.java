9009678b365f4ac44d103a7fddbaac7f8a7b7e80
pain <141949752+tadakatsv@users.noreply.github.com>
2026-06-23 22:59:45 +0300

    Rename OfferDtoRequest to OfferDtoPostRequest

--- src/main/java/ua/chekmaryov/barber_stat/dto/offers/OfferDtoPostRequest.java
+++ src/main/java/ua/chekmaryov/barber_stat/dto/offers/OfferDtoPostRequest.java
@@ -2,6 +2,6 @@ package ua.chekmaryov.barber_stat.dto.offers;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record OfferDtoRequest(
+public record OfferDtoPostRequest(
         @NotBlank(message = "Name is required") String name
 ){}
