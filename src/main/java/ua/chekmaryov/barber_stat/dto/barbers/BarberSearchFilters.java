6e37c3c8762c7ef67af153e8ed22fe40dfaaab5c
pain <141949752+tadakatsv@users.noreply.github.com>
2026-06-23 22:50:50 +0300

    Add search filter for Barber

---
+++ src/main/java/ua/chekmaryov/barber_stat/dto/barbers/BarberSearchFilters.java
@@ -0,0 +1,19 @@
+package ua.chekmaryov.barber_stat.dto.barbers;
+
+import lombok.Builder;
+import ua.chekmaryov.barber_stat.enums.BarberRole;
+import ua.chekmaryov.barber_stat.enums.BarberStatus;
+
+import java.time.LocalDate;
+
+@Builder
+public record BarberSearchFilters(
+        String firstName,
+        String lastName,
+        String phone,
+        LocalDate birthDate,
+        BarberStatus status,
+        BarberRole role,
+        Integer salaryPercent
+) {
+}
