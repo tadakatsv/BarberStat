10f6e9c0527a113e8f2352796f79f235be5c0680
pain <141949752+tadakatsv@users.noreply.github.com>
2026-06-23 22:52:46 +0300

    Add put request dto

---
+++ src/main/java/ua/chekmaryov/barber_stat/dto/clients/ClientDtoPutRequest.java
@@ -0,0 +1,30 @@
+package ua.chekmaryov.barber_stat.dto.clients;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
+import ua.chekmaryov.barber_stat.enums.ClientStatus;
+
+import java.time.LocalDate;
+
+@Builder
+public record ClientDtoPutRequest(
+        @NotBlank(message = "First name is required")
+        String firstName,
+        @NotBlank(message = "Last name is required")
+        String lastName,
+        @NotBlank(message = "Phone number is required")
+        @Pattern(regexp = "^\\d{9,12}$", message = "Invalid phone format")
+        String phone,
+        @NotNull(message = "Birth date is required")
+        @Past(message = "Birth date can't be in the future")
+        LocalDate birthDate,
+        @NotNull(message = "Status is required")
+        ClientStatus status,
+        @Past(message = "Last visit date can't be in the past")
+        LocalDate lastVisitDate,
+        String notes
+) {
+}
